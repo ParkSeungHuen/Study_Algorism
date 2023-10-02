fcffaf1b55500084ca1fa79a383dfabeb335485c
PSH980520 <qkrtmdgjs12@naver.com>
2023-10-02 21:49:31 +0900

    2013-10-02

---
+++ src/BackJoon/_11068.java
@@ -0,0 +1,58 @@
+package BackJoon;
+
+import java.io.*;
+import java.util.ArrayList;
+
+public class _11068 {
+    public static void _11068(String[] args) throws IOException { // 30분
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); // 버퍼리드 선언
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine()); // 몇개 입력받을 건지 입력
+
+        ArrayList<Integer> arr = new ArrayList<>();
+        boolean success = false;
+
+        for (int i = 0; i < N; i++) { // 입력받은 수 만큼 정수 입력받기
+            arr.add(Integer.parseInt(br.readLine()));
+        }
+
+        ArrayList<Integer> arrayList = new ArrayList<>();
+
+        for (int i = 0; i < arr.size(); i++) { // 입력받은 수 만큼 반복
+
+            for (int j = 2; j <= 64; j++) { // 2 ~ 64진법 까지 반복, 2 ~ 64진법 까지 반복했을 때 아무것도 충족하지 못할 경우
+                int num2 = arr.get(i);
+
+                while (num2 > 0) {
+                    arrayList.add(num2 % j); //n진법 변환 후 배열 추가
+                    num2 = num2 / j;
+                }
+
+                for (int k = 0; k < arrayList.size() / 2; k++) {
+                    if(arrayList.get(k) != arrayList.get(arrayList.size() - 1 - k)) {
+                        break;
+                    }
+
+                    if (k == (arrayList.size() / 2 - 1)) { // 만약 끝가지 갈 경우 성공
+                        success = true;
+                    }
+                }
+
+                arrayList.clear();
+            }
+
+            if (success == true) { // 판정
+                System.out.println("1");
+            } else {
+                System.out.println("0");
+            }
+
+            success = false;
+            arrayList.clear();
+        }
+
+        bw.flush();
+
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -3,30 +3,69 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException { // 45분, 이동은 구현했지만 + 변환 등은 하지 못함, 인강 ㄱㄱ
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); // 버퍼리드 선언
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        StringTokenizer tokenizer = new StringTokenizer(br.readLine());
+        int size = Integer.parseInt(br.readLine()); // 크기 입력받기
 
-        int num = Integer.parseInt(tokenizer.nextToken());
-        int B = Integer.parseInt(tokenizer.nextToken());
-        ArrayList<Integer> arrayList = new ArrayList<>();
+        char[][] chars = new char[size][size]; // 배열 선언
 
-        while (num > 0) {
-            arrayList.add((num % B));
-            num = num / B;
+        for (int i = 0; i < chars.length; i++) {
+            for (int j = 0; j < chars.length; j++) {
+                chars[i][j] = (char)46; // .
+            }
         }
 
-        for (int i = arrayList.size() - 1; i >= 0; i--) {
-            if(arrayList.get(i) > 9) {
-                System.out.print((char)(arrayList.get(i) + 55));
-            } else {
-                System.out.print(arrayList.get(i));
+        String drive = br.readLine();
+        int x = 0; // x좌표
+        int y = 0; // y좌표
+
+        for (int i = 0; i < drive.length(); i++) { // 입력받은 문자의 길이
+            String c = drive.substring(i,i+1);
+
+            switch (c) { // case 4가지 :  위쪽 이동 'U', 아래쪽으로 이동은 'D', 왼쪽으로 이동은 'L', 오른쪽으로 이동은 'R'
+
+                case "R":
+                    if (x != size) {
+                        chars[y][x] = (char)45;
+                        x++;
+                    }
+                    break;
+
+                case "L":
+                    if (x != 0) {
+                        chars[y][x] = (char)45;
+                        x--;
+                    }
+                    break;
+
+                case "D":
+                    if (y != size) {
+                        chars[y][x] = (char)124; // |
+                        y++;
+                    }
+                    break;
+
+                case "U":
+                    if (y != 0) {
+                        chars[y][x] = (char)124; // |
+                        y--;
+                    }
+                    break;
+
             }
+        }
+
+        for (int i = 0; i < chars.length; i++) {
+            for (int j = 0; j < chars.length; j++) {
+                System.out.print(chars[i][j]);
+            }
+            System.out.println();
         }
 
         bw.flush();
